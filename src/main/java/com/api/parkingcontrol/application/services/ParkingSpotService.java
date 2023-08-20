99f20763990c3896c0a45740009d1163321bbab2
tiago <tiagofmarante@gmail.com>
2023-08-20 22:49:22 +0100

    init project

---
+++ src/main/java/com/api/parkingcontrol/application/services/ParkingSpotService.java
@@ -0,0 +1,56 @@
+package com.api.parkingcontrol.application.services;
+
+import com.api.parkingcontrol.domain.models.ParkingSpotModel;
+import com.api.parkingcontrol.infrastructure.repositories.ParkingSpotRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class ParkingSpotService {
+
+    final ParkingSpotRepository parkingSpotRepository;
+
+    public ParkingSpotService(ParkingSpotRepository parkingSpotRepository) {
+        this.parkingSpotRepository = parkingSpotRepository;
+    }
+
+    @Transactional
+    public ParkingSpotModel save(ParkingSpotModel parkingSpotModel) {
+        return parkingSpotRepository.save(parkingSpotModel);
+    }
+
+    public boolean existsByLicensePlateCar(String licensePlateCar) {
+        System.out.println(parkingSpotRepository.findParkingSpotModelByBrandCar("audi"));
+
+        //System.out.println(parkingSpotRepository.findParkingSpotModelByBrandCar("mercedes"));
+
+        System.out.println(parkingSpotRepository.findParkingSpotModelByBrandCar("mercedes"));
+        return parkingSpotRepository.existsByLicensePlateCar(licensePlateCar);
+    }
+
+    public boolean existsByParkingSpotNumber(String parkingSpotNumber) {
+        return parkingSpotRepository.existsByParkingSpotNumber(parkingSpotNumber);
+    }
+
+    public boolean existsByApartmentAndBlock(String apartment, String block) {
+        return parkingSpotRepository.existsByApartmentAndBlock(apartment, block);
+    }
+
+    public Page<ParkingSpotModel> findAll(Pageable pageable) {
+        return parkingSpotRepository.findAll(pageable);
+    }
+
+    public Optional<ParkingSpotModel> findById(UUID id) {
+        return parkingSpotRepository.findById(id);
+    }
+
+    @Transactional
+    public void delete(ParkingSpotModel parkingSpotModel) {
+        parkingSpotRepository.delete(parkingSpotModel);
+    }
+}
